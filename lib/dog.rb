249b9e5acecf58d0d347f5c2e8f5dcc34310a63a
Chris Gullickson <cgullickson5@gmail.com>
2018-01-05 20:59:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,12 +15,12 @@ class Dog
              breed TEXT
              )
          SQL
-   
+
      DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL 
+    sql = <<-SQL
       DROP TABLE IF EXISTS dogs
     SQL
 
